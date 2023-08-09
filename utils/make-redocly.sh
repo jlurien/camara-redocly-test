858018d2f0095f67724355122a1254f7f0c21500
Jose Luis Urien <jlurien@gmail.com>
2023-08-09 11:55:42 +0200

    Add version to filename

--- utils/make-redocly.sh
+++ utils/make-redocly.sh
@@ -5,8 +5,10 @@ echo "Getting started"
 # Build docs for any yaml file in the directory "./code/API_definitions" into a zero-dependency HTML file with the same filename
 for file in ./code/API_definitions/*.yaml
 do
-    echo "Building $file"
-    npx @redocly/cli build-docs $file -o ./$(basename $file .yaml).html
+    # Read info.version from the yaml file and use it as the version for the HTML file
+    version=$(grep "info:" $file | sed -n 's/\s*version:\s*//p')
+    echo "Building $file for version $version"
+    npx @redocly/cli build-docs $file -o ./$(basename $file .yaml)_$(version).html
 done
 
 echo -e "\nDone!"
